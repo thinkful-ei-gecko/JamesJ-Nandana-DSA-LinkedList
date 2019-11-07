969d870353b7780657b91f7657c76ce729496622
nrrao <nandanarao@yahoo.com>
2019-11-07 12:48:17 -0500

    finished singly linked list

--- exercise.js
+++ exercise.js
@@ -1,7 +1,7 @@
 class _Node {
   constructor(value, next) {
-    this.value = value
-    this.next = next
+    this.value = value;
+    this.next = next;
   }
 }
 // 1. Create a linked list class
@@ -11,45 +11,116 @@ class _Node {
 
 class LinkedList {
   constructor() {
-    this.head = null
+    this.head = null;
   }
 
   insertFirst(value) {
-    this.head = new _Node(value, this.head)
+    this.head = new _Node(value, this.head);
   }
 
   insertLast(value) {
-    let tempNode = this.head
+    let tempNode = this.head;
     while(tempNode.next !== null) {
-      tempNode = tempNode.next
+      tempNode = tempNode.next;
     }
-    tempNode.next = new _Node(value, null)
+    tempNode.next = new _Node(value, null);
   }
 
   remove(value) {
-    let prevNode = null
-    let currentNode = this.head
+    let prevNode = null;
+    let currentNode = this.head;
     while(currentNode.value !== value) {
-      prevNode = currentNode
-      currentNode = currentNode.next
+      prevNode = currentNode;
+      currentNode = currentNode.next;
+      if(currentNode === null){
+        console.log('Item not found');
+        return;
+      }
     }
-    prevNode.next = currentNode.next
+    if(currentNode != null){
+      prevNode.next = currentNode.next;
+    }
+    
   }
 
   find(value) {
-    let tempNode = this.head
-    while (tempNode.value !== value) {
-      tempNode = tempNode.next
+    let tempNode = this.head;
+    if(tempNode != null){
+      while (tempNode.value !== value) {
+        tempNode = tempNode.next;
+        if(tempNode === null){
+          return;
+        }
+      }
+      return tempNode;
     }
-    return tempNode
+    else{
+      console.log('Value not found');
+      return;
+    }
+
   }
+
+  insertBefore(value,nodeValue){
+    let prevNode = null;
+    let currentNode = this.head;
+    
+    while(currentNode.value != nodeValue){
+      prevNode = currentNode;
+      currentNode = currentNode.next;
+      
+    }
+    if(currentNode != null){
+      prevNode.next = new _Node(value,currentNode);
+    }
+
+  }
+
+  insertAfter(value,nodeValue){
+    
+    let currentNode = this.head;
+    let nextNode = currentNode.next;
+    
+    while(currentNode.value != nodeValue){
+      
+      currentNode = nextNode;
+      nextNode = currentNode.next;
+    }
+    currentNode.next = new _Node(value,nextNode)
+
+  }
+  insertAt(value,nodeIndex){
+    let prevNode = null;
+    let currentNode = this.head;
+    let counter = 0;
+    while(counter < nodeIndex ){
+      prevNode = currentNode;
+      currentNode = currentNode.next;
+      counter++;
+    }
+    if(currentNode != null){
+      prevNode.next = new _Node(value,currentNode);
+    }
+
+  }
+
 }
 
 function main() {
-  let testLL = new LinkedList
-  testLL.insertFirst('foo')
-  testLL.insertLast('bar')
-  testLL.insertLast('remove')
+  let SLL = new LinkedList;
+  SLL.insertFirst('Apollo');
+  SLL.insertLast('Boomer');
+  SLL.insertLast('Helo');
+  SLL.insertLast('Husker');
+  SLL.insertLast('Starbuck');
+  SLL.insertLast('Tauhida');
+  
+  SLL.remove('squirrel');
+  SLL.insertBefore('Athena','Boomer');
+  SLL.insertAfter('Hotdog','Helo');
+  SLL.insertAt('Kat',2);
+  SLL.remove('Tauhida');
+  console.log(SLL.find('Tauhida'));
 }
 
-main()
+main();
