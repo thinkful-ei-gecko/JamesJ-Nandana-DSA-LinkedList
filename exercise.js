fb7656ef9b12ca533d4dc763e83e339c5b88d6b2
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-11-07 11:25:53 -0500

    starting point

---
+++ exercise.js
@@ -0,0 +1,10 @@
+class _NODE {
+  constructor(value, next) {
+    this.value = value
+    this.next = next
+  }
+}
+// 1. Create a linked list class
+// Walk through the linked list code in the curriculum and understand it well. 
+// Then write a linked list class and its core functions 
+// (insertFirst, insertLast, remove, find) from scratch.
